7a7650b7343c468659d2d87d3fb6f99e8eb15550
trungdang97 <35669562+trungdang97@users.noreply.github.com>
2018-03-19 15:23:01 +0700

    tạo form cập nhật và tạo hàm thực hiện thủ tục cập nhật

--- WF_QuanLyNhanSu/QuanLyNhanSu/db_connection.cs
+++ WF_QuanLyNhanSu/QuanLyNhanSu/db_connection.cs
@@ -27,5 +27,26 @@ namespace WindowsFormsApplication1
             }
             return data;
         }
+
+        //update thong tin nhan su
+        public static void updateThongtincanhan(string manv,string tennv, string cmnd, string sdt, string diachi, string ngaysinh, string maphong, int tdhv)
+        {
+            using (SqlConnection connection = new SqlConnection(connection_string.ConnectionString))
+            {
+                SqlCommand query = new SqlCommand("exec updateThongtincanhan @manv, @tennv, @cmnd, @sdt, @diachi, @ngaysinh, @maphong, @tdhv");
+                query.Parameters.Add("@manv", SqlDbType.VarChar, 10);
+                query.Parameters.Add("@tennv", SqlDbType.NVarChar, 50);
+                query.Parameters.Add("@cmnd", SqlDbType.VarChar, 10);
+                query.Parameters.Add("@sdt", SqlDbType.VarChar, 11);
+                query.Parameters.Add("@diachi", SqlDbType.NVarChar, 10);
+                query.Parameters.Add("@ngaysinh", SqlDbType.Date, 10);
+                query.Parameters.Add("@maphong", SqlDbType.VarChar, 10);
+                query.Parameters.Add("@tdhv", SqlDbType.Int);
+
+                connection.Open();
+                query.ExecuteNonQuery();
+                connection.Close();
+            }         
+        }
     }
 }
---
+++ WF_QuanLyNhanSu/QuanLyNhanSu/update_form.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class update_form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_name = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lb_id = new System.Windows.Forms.Label();
+            this.lb_cmnd = new System.Windows.Forms.Label();
+            this.lb_phone = new System.Windows.Forms.Label();
+            this.lb_birth = new System.Windows.Forms.Label();
+            this.lb_address = new System.Windows.Forms.Label();
+            this.lb_department = new System.Windows.Forms.Label();
+            this.lb_diploma = new System.Windows.Forms.Label();
+            this.txtDepartment = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtCMND = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.lb_id_string = new System.Windows.Forms.Label();
+            this.cbMonth = new System.Windows.Forms.ComboBox();
+            this.cbDiploma = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // lb_name
+            // 
+            this.lb_name.AutoSize = true;
+            this.lb_name.Location = new System.Drawing.Point(11, 213);
+            this.lb_name.Name = "lb_name";
+            this.lb_name.Size = new System.Drawing.Size(57, 13);
+            this.lb_name.TabIndex = 0;
+            this.lb_name.Text = "Họ và tên:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 1;
+            // 
+            // lb_id
+            // 
+            this.lb_id.AutoSize = true;
+            this.lb_id.Location = new System.Drawing.Point(12, 187);
+            this.lb_id.Name = "lb_id";
+            this.lb_id.Size = new System.Drawing.Size(75, 13);
+            this.lb_id.TabIndex = 2;
+            this.lb_id.Text = "Mã nhân viên:";
+            // 
+            // lb_cmnd
+            // 
+            this.lb_cmnd.AutoSize = true;
+            this.lb_cmnd.Location = new System.Drawing.Point(12, 265);
+            this.lb_cmnd.Name = "lb_cmnd";
+            this.lb_cmnd.Size = new System.Drawing.Size(42, 13);
+            this.lb_cmnd.TabIndex = 3;
+            this.lb_cmnd.Text = "CMND:";
+            // 
+            // lb_phone
+            // 
+            this.lb_phone.AutoSize = true;
+            this.lb_phone.Location = new System.Drawing.Point(12, 291);
+            this.lb_phone.Name = "lb_phone";
+            this.lb_phone.Size = new System.Drawing.Size(32, 13);
+            this.lb_phone.TabIndex = 4;
+            this.lb_phone.Text = "SĐT:";
+            // 
+            // lb_birth
+            // 
+            this.lb_birth.AutoSize = true;
+            this.lb_birth.Location = new System.Drawing.Point(12, 239);
+            this.lb_birth.Name = "lb_birth";
+            this.lb_birth.Size = new System.Drawing.Size(57, 13);
+            this.lb_birth.TabIndex = 5;
+            this.lb_birth.Text = "Ngày sinh:";
+            // 
+            // lb_address
+            // 
+            this.lb_address.AutoSize = true;
+            this.lb_address.Location = new System.Drawing.Point(11, 319);
+            this.lb_address.Name = "lb_address";
+            this.lb_address.Size = new System.Drawing.Size(43, 13);
+            this.lb_address.TabIndex = 6;
+            this.lb_address.Text = "Địa chỉ:";
+            // 
+            // lb_department
+            // 
+            this.lb_department.AutoSize = true;
+            this.lb_department.Location = new System.Drawing.Point(11, 345);
+            this.lb_department.Name = "lb_department";
+            this.lb_department.Size = new System.Drawing.Size(71, 13);
+            this.lb_department.TabIndex = 7;
+            this.lb_department.Text = "Nơi công tác:";
+            // 
+            // lb_diploma
+            // 
+            this.lb_diploma.AutoSize = true;
+            this.lb_diploma.Location = new System.Drawing.Point(11, 371);
+            this.lb_diploma.Name = "lb_diploma";
+            this.lb_diploma.Size = new System.Drawing.Size(92, 13);
+            this.lb_diploma.TabIndex = 8;
+            this.lb_diploma.Text = "Trình độ học vấn:";
+            // 
+            // txtDepartment
+            // 
+            this.txtDepartment.Location = new System.Drawing.Point(111, 342);
+            this.txtDepartment.Name = "txtDepartment";
+            this.txtDepartment.Size = new System.Drawing.Size(250, 20);
+            this.txtDepartment.TabIndex = 10;
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(111, 316);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(250, 20);
+            this.txtAddress.TabIndex = 11;
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(111, 288);
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(250, 20);
+            this.txtSDT.TabIndex = 12;
+            // 
+            // txtCMND
+            // 
+            this.txtCMND.Location = new System.Drawing.Point(111, 262);
+            this.txtCMND.Name = "txtCMND";
+            this.txtCMND.Size = new System.Drawing.Size(250, 20);
+            this.txtCMND.TabIndex = 13;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(111, 210);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(250, 20);
+            this.txtName.TabIndex = 15;
+            // 
+            // txtDay
+            // 
+            this.txtDay.Location = new System.Drawing.Point(111, 235);
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(86, 20);
+            this.txtDay.TabIndex = 17;
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(281, 236);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(80, 20);
+            this.txtYear.TabIndex = 18;
+            // 
+            // lb_id_string
+            // 
+            this.lb_id_string.AutoSize = true;
+            this.lb_id_string.Location = new System.Drawing.Point(268, 187);
+            this.lb_id_string.Name = "lb_id_string";
+            this.lb_id_string.Size = new System.Drawing.Size(93, 13);
+            this.lb_id_string.TabIndex = 19;
+            this.lb_id_string.Text = "id_is_placed_here";
+            // 
+            // cbMonth
+            // 
+            this.cbMonth.FormattingEnabled = true;
+            this.cbMonth.Location = new System.Drawing.Point(203, 235);
+            this.cbMonth.Name = "cbMonth";
+            this.cbMonth.Size = new System.Drawing.Size(72, 21);
+            this.cbMonth.TabIndex = 20;
+            // 
+            // cbDiploma
+            // 
+            this.cbDiploma.FormattingEnabled = true;
+            this.cbDiploma.Location = new System.Drawing.Point(111, 368);
+            this.cbDiploma.Name = "cbDiploma";
+            this.cbDiploma.Size = new System.Drawing.Size(250, 21);
+            this.cbDiploma.TabIndex = 21;
+            // 
+            // update_form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 408);
+            this.Controls.Add(this.cbDiploma);
+            this.Controls.Add(this.cbMonth);
+            this.Controls.Add(this.lb_id_string);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtCMND);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtDepartment);
+            this.Controls.Add(this.lb_diploma);
+            this.Controls.Add(this.lb_department);
+            this.Controls.Add(this.lb_address);
+            this.Controls.Add(this.lb_birth);
+            this.Controls.Add(this.lb_phone);
+            this.Controls.Add(this.lb_cmnd);
+            this.Controls.Add(this.lb_id);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lb_name);
+            this.Name = "update_form";
+            this.Text = "Sửa thông tin cá nhân";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_name;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lb_id;
+        private System.Windows.Forms.Label lb_cmnd;
+        private System.Windows.Forms.Label lb_phone;
+        private System.Windows.Forms.Label lb_birth;
+        private System.Windows.Forms.Label lb_address;
+        private System.Windows.Forms.Label lb_department;
+        private System.Windows.Forms.Label lb_diploma;
+        private System.Windows.Forms.TextBox txtDepartment;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtCMND;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label lb_id_string;
+        private System.Windows.Forms.ComboBox cbMonth;
+        private System.Windows.Forms.ComboBox cbDiploma;
+    }
+}
